6b5f585ecaceda141f5517855eb3fd5143afd9e5
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-08-19 08:40:40 +0500

    Add second commit

--- MyPortfolio/src/app/app.module.ts
+++ MyPortfolio/src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {RecentProjectsComponent} from "./core/recent-projects/recent-projects.component";
 import { IntroductionComponent } from './core/introduction/introduction.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        RecentProjectsComponent,
-        RecentProjectsComponent,
         IntroductionComponent
     ],
   imports: [
